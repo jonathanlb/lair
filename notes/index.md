f80fe15d6405687599222eb82d87201e7a8e6bf7
Jonathan Bredin <bredin@acm.org>
2021-01-30 13:15:22 -0600

    convergence notes/scepticism

---
+++ notes/index.md
@@ -0,0 +1,2 @@
+# Notes
+- [2021-01-30](2021-01-30.md) Train to fit a quadratic function.
--- src/bin/fit_quad.rs
+++ src/bin/fit_quad.rs
@@ -5,9 +5,31 @@ use na::{Matrix, Matrix1, Matrix2x1};
 use na::{U1, U2};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "fit quadratic example",
+    about = "Demonstrate batch convergence training on a simple function."
+)]
+struct OptimizeParams {
+    #[structopt(short = "n", long = "iter", default_value = "100")]
+    max_iter: usize,
+    #[structopt(short = "s", long = "step", default_value = "1e-3")]
+    step_size: Fxx,
+    #[structopt(short = "b", long = "test_size", default_value = "20")]
+    test_batch: usize,
+    #[structopt(short = "B", long = "train_batch", default_value = "80")]
+    train_batch: usize,
+}
 
 fn f(x: &Matrix2x1<Fxx>) -> Matrix1<Fxx> {
-    Matrix1::<Fxx>::new(0.5 * x[0] - 4.0 * x[1] - 6.0)
+    // Matrix1::<Fxx>::new(0.5 * x[0] - 4.0 * x[1] - 6.0)
+    Matrix1::<Fxx>::new(
+        0.5 * x[0] * x[0] 
+        + 2.0 * x[0] * x[1] 
+        - 4.0 * x[1] * x[1] 
+        - 6.0)
 }
 
 fn sample_input(sz: usize) -> Vec<Matrix2x1<Fxx>> {
@@ -25,15 +47,10 @@ fn sample_input(sz: usize) -> Vec<Matrix2x1<Fxx>> {
         .collect()
 }
 
-struct OptimizeParams {
-    max_iter: usize,
-    step_size: Fxx,
-    test_batch: usize,
-    train_batch: usize,
-}
-
 fn optimize_quadratic(params: &OptimizeParams) {
-    let learning_rate = UpdateParams { step_size: params.step_size };
+    let learning_rate = UpdateParams {
+        step_size: params.step_size,
+    };
 
     let mut m0 = LinearModel::<U2, U2>::new_random(&learning_rate);
     let mut m1 = LinearModel::<U2, U1>::new_random(&learning_rate);
@@ -53,7 +70,7 @@ fn optimize_quadratic(params: &OptimizeParams) {
             .map(|x| Matrix::norm(&(f(x) - model.predict(x))))
             .sum::<Fxx>()
             / (params.test_batch as Fxx);
-        println!("{} {}", i*params.train_batch, err);
+        println!("{} {}", i * params.train_batch, err);
     }
 }
 
@@ -62,12 +79,10 @@ fn optimize_quadratic(params: &OptimizeParams) {
 //
 fn main() {
     env_logger::init();
-    let params = OptimizeParams {
-        max_iter: 100,
-        step_size: 1e-2,
-        test_batch: 20,
-        train_batch: 80,
-    };
-    println!("# step={} n_train={} n_test={}", params.step_size, params.train_batch, params.test_batch);
+    let params = OptimizeParams::from_args();
+    println!(
+        "# step={} n_train={} n_test={}",
+        params.step_size, params.train_batch, params.test_batch
+    );
     optimize_quadratic(&params);
 }
